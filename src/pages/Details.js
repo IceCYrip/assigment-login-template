255fb8b4dc09cd225ad3e2157e4db2f408c84198
IceCYrip <karansable16@gmail.com>
2024-08-25 12:16:47 +0530

    🐛 added missing imports

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -5,6 +5,7 @@ import Admin from './components/Admin'
 import User from './components/User'
 import { Button, Paper } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
+import sweetAlert from 'sweetalert'
 
 const Details = () => {
   const token = sessionStorage.getItem('token')
--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import sweetAlert from 'sweetalert'
 
 const errorHandler = (err) => {
   console.log(err)
